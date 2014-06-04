07fee841b28c1a0bd76f62d2b42824036ab463aa
Michael Hoffman <mhoffman999@gmail.com>
2014-06-04 18:56:11 -0400

    Committing crimes.

---
+++ SortDescriptor/MJHAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MJHAppDelegate.h
+//  SortDescriptor
+//
+//  Created by Michael Hoffman on 6/4/14.
+//  Copyright (c) 2014 Here We Go. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MJHAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SortDescriptor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SortDescriptor
+//
+//  Created by Michael Hoffman on 6/4/14.
+//  Copyright (c) 2014 Here We Go. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "MJHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([MJHAppDelegate class]));
+    }
+}
